61e82a5a8c2a1df3ea1c3a660ac1452bed243b45
MatheusBR0110 <matheus.fernandes01@escola.pr.gov.br>
2026-04-08 11:53:13 -0300

    Add files via upload

---
+++ nivelamento.js
@@ -0,0 +1,32 @@
+//Matheus Gonçalves Fernandes 3°TEC
+
+//1. Exiba a mensagem "Olá mundo" no console.
+console.log("Olá Mundo");
+
+//2. Crie uma variável com seu nome e exiba o valor no console.
+let nome = "Matheus";
+console.log(nome);
+
+//3. Some dois números e exiba o resultado
+let x = 5;
+let y = 3;
+console.log(x + y);
+
+//4. Subtraia doi numeros e exiba o valor.
+let subtraia = 50 - 29;
+console.log(subtraia);
+
+//5. escubra o resto da divisão de um numero por outro.
+let resto = 27%18;
+console.log(resto);
+
+//6. Crie uma variável booleana(true false) e exiba o valor.
+let bool = true;
+console.log(bool);
+
+//7. Verifique se um número é maior que outro e exiba o resultado.
+if(50 - 29){
+    console.log("É maior")
+}
+
+//8. 
